940d2177af2dc3b7f7774897d167c6c94bd7a412
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2025-03-09 21:27:45 +0100

    created first start function

--- API_task.mjs
+++ API_task.mjs
@@ -3,7 +3,15 @@ import { futimesSync } from 'fs';
 const fetch = require('node-fetch');
 
 const PLAYER_EMAIL = 'aleksandnb@uia.no';
+
 const SOLAR_SYSTEM_API_URL = 'https://api.le-systeme-solaire.net/en/';
 const RIS_BASE_URL = 'https://spacescavanger.onrender.com/';
 
-push
+async function startTask() {
+    const response = await fetch(`${RIS_BASE_URL}/start?player=${PLAYER_EMAIL}`);
+    const data = await response.json();
+    console.log('Task started:', data);
+    return data;
+}
+
+
