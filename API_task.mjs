85ca4a9643f316d2d8bf4809a0a7ea64a6b685df
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2025-03-09 21:25:08 +0100

    Started with consts and config

--- API_task.mjs
+++ API_task.mjs
@@ -3,10 +3,6 @@ import { futimesSync } from 'fs';
 const fetch = require('node-fetch');
 
 const PLAYER_EMAIL = 'aleksandnb@uia.no';
-
 const SOLAR_SYSTEM_API_URL = 'https://api.le-systeme-solaire.net/en/';
 const RIS_BASE_URL = 'https://spacescavanger.onrender.com/';
-
-
-
 
